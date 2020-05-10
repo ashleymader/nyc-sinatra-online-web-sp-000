8058dc8738f6307b294d05515b8c18e1393905de
ashleymader <ashley.mader12@gmail.com>
2020-05-10 16:15:42 -0400

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,19 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  
+  get '/landmarks' do 
+    @landmarks = Landmark.all 
+    erb :"landmarks/index"
+  end
+
+  get '/landmarks/new' do 
+    erb :"landmarks/new"
+  end
+
+  post '/landmarks' do 
+    Landmark.create(name: params['landmark']['name'], year_completed: params['landmark']['year_completed'])
+    redirect '/landmarks'
+  end
+
+  
 end
